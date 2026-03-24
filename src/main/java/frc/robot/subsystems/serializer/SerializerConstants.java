b8f751f77bc0edea6117f50e5a38e80271c00281
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2026-03-24 18:37:11 -0400

    Updated Serializer Constants to speed it up (#82)

--- src/main/java/frc/robot/subsystems/serializer/SerializerConstants.java
+++ src/main/java/frc/robot/subsystems/serializer/SerializerConstants.java
@@ -5,7 +5,7 @@ import edu.wpi.first.units.measure.Current;
 import static edu.wpi.first.units.Units.Amps;
 
 public final class SerializerConstants {
-    public static final double  SERIALIZING_VOLTAGE = 10;
-    public static final Current CURRENT_LIMIT       = Amps.of(20);
+    public static final double  SERIALIZING_VOLTAGE = 12;
+    public static final Current CURRENT_LIMIT       = Amps.of(30);
     public static final double  GEARING             = 1.0 / 3.0;
 }
